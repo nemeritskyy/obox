e1508adf40480ab2df192288e8862cf928cda3eb
nemeritskyy <85497703+nemeritskyy@users.noreply.github.com>
2023-06-23 17:48:20 +0300

    initial project

---
+++ src/main/java/ua/com/obox/OboxApplication.java
@@ -0,0 +1,13 @@
+package ua.com.obox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OboxApplication.class, args);
+    }
+
+}
