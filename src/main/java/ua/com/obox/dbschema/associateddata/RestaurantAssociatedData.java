5e8360a9f45742e7f87de4b24e0a65297666dd63
nemeritskyy <85497703+nemeritskyy@users.noreply.github.com>
2023-08-01 06:46:09 +0300

    Add multi-language allergens and tags: ua, en, es, fr, de, it, pt, ru, zh, ja

--- src/main/java/ua/com/obox/dbschema/associateddata/RestaurantAssociatedData.java
+++ src/main/java/ua/com/obox/dbschema/associateddata/RestaurantAssociatedData.java
@@ -14,7 +14,6 @@ import java.util.stream.Collectors;
 @AllArgsConstructor
 @Entity
 @ToString
-@Table(name = "RestaurantAssociatedData")
 public class RestaurantAssociatedData {
     @Id
     @GeneratedValue(generator = "uuid2")
@@ -38,64 +37,55 @@ public class RestaurantAssociatedData {
 
     @JsonIgnore
     public String setDefaultAllergens(String languageCode) {
-        List<String> allergens = new ArrayList<>();
-        if (languageCode.equals("ua")) {
-            allergens.add("Крупи, що містять глютен");
-            allergens.add("Ракоподібні");
-            allergens.add("Яйця");
-            allergens.add("Риба");
-            allergens.add("Арахіс");
-            allergens.add("Соя");
-            allergens.add("Молоко");
-            allergens.add("Горіхи");
-            allergens.add("Селера");
-            allergens.add("Гірчиця");
-            allergens.add("Кунжут");
-            allergens.add("Двоокис сірки та сульфіти");
-            allergens.add("Люпин");
-            allergens.add("Молюски");
-            allergens.add("Мед");
-        }
-        if (languageCode.equals("en")) {
-            allergens.add("Cereals containing gluten");
-            allergens.add("Shellfish");
-            allergens.add("Eggs");
-            allergens.add("Fish");
-            allergens.add("Peanut");
-            allergens.add("Soy");
-            allergens.add("Milk");
-            allergens.add("Nuts");
-            allergens.add("Celery");
-            allergens.add("Mustard");
-            allergens.add("Sesame");
-            allergens.add("Sulfur dioxide and sulfites");
-            allergens.add("Lupine");
-            allergens.add("Clams");
-            allergens.add("Honey");
-        }
+        List<String> allergens = switch (languageCode) {
+            case "ua" -> List.of("Крупи, що містять глютен", "Ракоподібні", "Яйця", "Риба", "Арахіс", "Соя", "Молоко",
+                    "Горіхи", "Селера", "Гірчиця", "Кунжут", "Двоокис сірки та сульфіти", "Люпин", "Молюски", "Мед");
+            case "en" ->
+                    List.of("Cereals containing gluten", "Crustaceans", "Eggs", "Fish", "Peanuts", "Soybeans", "Milk",
+                            "Nuts", "Celery", "Mustard", "Sesame seeds", "Sulphur dioxide and sulphites", "Lupin", "Molluscs", "Honey");
+            case "es" ->
+                    List.of("Cereales que contienen gluten", "Crustáceos", "Huevos", "Pescado", "Cacahuetes", "Soja",
+                            "Leche", "Frutos secos", "Apio", "Mostaza", "Semillas de sésamo", "Dióxido de azufre y sulfitos", "Altramuces", "Moluscos", "Miel");
+            case "fr" -> List.of("Céréales contenant du gluten", "Crustacés", "Œufs", "Poisson", "Arachides", "Soja",
+                    "Lait", "Fruits à coque", "Céleri", "Moutarde", "Graines de sésame", "Dioxyde de soufre et sulfites", "Lupin", "Mollusques", "Miel");
+            case "de" ->
+                    List.of("Glutenhaltige Getreide", "Krebstiere", "Eier", "Fisch", "Erdnüsse", "Sojabohnen", "Milch",
+                            "Schalenfrüchte", "Sellerie", "Senf", "Sesamsamen", "Schwefeldioxid und Sulphite", "Lupinen", "Mollusken", "Honig");
+            case "it" ->
+                    List.of("Cereali contenenti glutine", "Crostacei", "Uova", "Pesce", "Arachidi", "Soia", "Latte",
+                            "Frutta a guscio", "Sedano", "Senape", "Semi di sesamo", "Anidride solforosa e solfiti", "Lupini", "Molluschi", "Miele");
+            case "pt" -> List.of("Cereais contendo glúten", "Crustáceos", "Ovos", "Peixe", "Amendoins", "Soja", "Leite",
+                    "Frutos de casca rija", "Aipo", "Mostarda", "Sementes de sésamo", "Dióxido de enxofre e sulfitos", "Lupinos", "Moluscos", "Mel");
+            case "ru" -> List.of("Крупы, содержащие глютен", "Ракообразные", "Яйца", "Рыба", "Арахис", "Соя", "Молоко",
+                    "Орехи", "Сельдерей", "Горчица", "Кунжут", "Двуокись серы и сульфиты", "Люпин", "Моллюски", "Мед");
+            case "zh", "cn" ->
+                    List.of("含麸质的谷类", "甲壳类动物", "鸡蛋", "鱼", "花生", "大豆", "牛奶", "坚果", "芹菜", "芥末", "芝麻籽", "二氧化硫和亚硫酸盐", "蜀葵", "软体动物", "蜜蜂蜜");
+            case "ja" ->
+                    List.of("グルテン含有穀物", "甲殻類", "卵", "魚", "ピーナッツ", "大豆", "乳", "ナッツ", "セロリ", "マスタード", "ゴマの種", "二酸化硫黄と亜硫酸塩", "ルピン", "貝類", "蜂蜜");
+            default -> Collections.emptyList();
+        };
         return allergens.stream().sorted()
                 .collect(Collectors.joining("::"));
     }
 
+
     @JsonIgnore
     public String setDefaultTags(String languageCode) {
-        List<String> tags = new ArrayList<>();
-        if (languageCode.equals("ua")) {
-            tags.add("Вегетаріанський");
-            tags.add("Не містить глютену");
-            tags.add("Гостре");
-            tags.add("Середньо-гострий");
-            tags.add("Рекомендовано");
-            tags.add("Веганське");
-        }
-        if (languageCode.equals("en")) {
-            tags.add("Vegetarian");
-            tags.add("Does not contain gluten");
-            tags.add("Spicy");
-            tags.add("Medium-spicy");
-            tags.add("Recommended");
-            tags.add("Vegan");
-        }
+        List<String> tags = switch (languageCode) {
+            case "ua" ->
+                    List.of("Вегетаріанський", "Не містить глютену", "Гостре", "Середньо-гострий", "Рекомендовано", "Веганське");
+            case "en" -> List.of("Vegetarian", "Gluten-free", "Spicy", "Medium-spicy", "Recommended", "Vegan");
+            case "es" -> List.of("Vegetariano", "Sin gluten", "Picante", "Medio picante", "Recomendado", "Vegano");
+            case "fr" -> List.of("Végétarien", "Sans gluten", "Épicé", "Moyennement épicé", "Recommandé", "Végétalien");
+            case "de" -> List.of("Vegetarisch", "Glutenfrei", "Scharf", "Mittel-scharf", "Empfohlen", "Vegan");
+            case "it" -> List.of("Vegetariano", "Senza glutine", "Piccante", "Medio piccante", "Consigliato", "Vegano");
+            case "pt" -> List.of("Vegetariano", "Sem glúten", "Picante", "Meio picante", "Recomendado", "Vegano");
+            case "ru" ->
+                    List.of("Вегетарианский", "Без глютена", "Острый", "Средне-острый", "Рекомендуемое", "Веганское");
+            case "zh", "cn" -> List.of("素食", "无麸质", "辣", "中辣", "推荐", "纯素");
+            case "ja" -> List.of("ベジタリアン", "グルテンフリー", "辛い", "中辛い", "おすすめ", "ビーガン");
+            default -> Collections.emptyList();
+        };
         return tags.stream().sorted()
                 .collect(Collectors.joining("::"));
     }
