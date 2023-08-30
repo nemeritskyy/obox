cd7787ccd0efea6c9517eb6f2e5cd014599d1173
nemeritskyy <85497703+nemeritskyy@users.noreply.github.com>
2023-08-30 09:31:10 +0300

    rename to images, support base64 with metadata, fix arrays allergens and tags, delete images from server if delete upper entity

--- src/main/java/ua/com/obox/dbschema/dish/DishController.java
+++ src/main/java/ua/com/obox/dbschema/dish/DishController.java
@@ -21,37 +21,39 @@ public class DishController {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Success", content = @Content(mediaType = "application/json",
                     schema = @Schema(example =
-                            "{\n" +
-                                    "    \"dish_id\": \"762dbe8e-4cfc-40f0-af3f-02cfc67da709\",\n" +
-                                    "    \"category_id\": \"decb23a3-b437-4c28-95df-a93aa59d90a7\",\n" +
-                                    "    \"associated_id\": \"915e9be8-a36b-40d9-bf9b-aeedc180958e\",\n" +
-                                    "    \"name\": \"Dorblue\",\n" +
-                                    "    \"description\": \"string\",\n" +
-                                    "    \"price\": 300.0,\n" +
-                                    "    \"weight\": 250,\n" +
-                                    "    \"calories\": 400,\n" +
-                                    "    \"allergens\": [\n" +
-                                    "        \"Celery\",\n" +
-                                    "        \"Cereals containing gluten\",\n" +
-                                    "        \"Clams\"\n" +
-                                    "    ],\n" +
-                                    "    \"tags\": [\n" +
-                                    "        \"Spicy\",\n" +
-                                    "        \"Vegan\"\n" +
-                                    "    ],\n" +
-                                    "    \"image_url\": \"https://img.obox.com.ua/915e9be8-a36b-40d9-bf9b-aeedc180958e/762dbe8e-4cfc-40f0-af3f-02cfc67da709.jpg\",\n" +
-                                    "    \"state\": \"ENABLED\"\n" +
-                                    "}"
+                            """
+                                    {
+                                        "dish_id": "762dbe8e-4cfc-40f0-af3f-02cfc67da709",
+                                        "category_id": "decb23a3-b437-4c28-95df-a93aa59d90a7",
+                                        "associated_id": "915e9be8-a36b-40d9-bf9b-aeedc180958e",
+                                        "name": "Dorblue",
+                                        "description": "string",
+                                        "price": 300.0,
+                                        "weight": 250,
+                                        "calories": 400,
+                                        "allergens": [
+                                            "Celery",
+                                            "Cereals containing gluten",
+                                            "Clams"
+                                        ],
+                                        "tags": [
+                                            "Spicy",
+                                            "Vegan"
+                                        ],
+                                        "image_url": "https://img.obox.com.ua/915e9be8-a36b-40d9-bf9b-aeedc180958e/762dbe8e-4cfc-40f0-af3f-02cfc67da709.jpg",
+                                        "state": "ENABLED"
+                                    }"""
                     ))),
             @ApiResponse(responseCode = "404", description = "Not found", content = @Content(mediaType = "application/json",
                     schema = @Schema(example =
-                            "{\n" +
-                                    "    \"timestamp\": \"2023-08-23T08:00:50.333+00:00\",\n" +
-                                    "    \"status\": 404,\n" +
-                                    "    \"error\": \"Not Found\",\n" +
-                                    "    \"message\": \"Dish with id 762dbe8e-4cxc-40f0-af3f-02cfc67da709 not found\",\n" +
-                                    "    \"path\": \"/dishes/762dbe8e-4cxc-40f0-af3f-02cfc67da709\"\n" +
-                                    "}"
+                            """
+                                    {
+                                        "timestamp": "2023-08-23T08:00:50.333+00:00",
+                                        "status": 404,
+                                        "error": "Not Found",
+                                        "message": "Dish with id 762dbe8e-4cxc-40f0-af3f-02cfc67da709 not found",
+                                        "path": "/dishes/762dbe8e-4cxc-40f0-af3f-02cfc67da709"
+                                    }"""
                     )))
     })
     public ResponseEntity<DishResponse> getDishById(@PathVariable String dishId) {
@@ -64,47 +66,50 @@ public class DishController {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "Success", content = @Content(mediaType = "application/json",
                     schema = @Schema(example =
-                            "{\n" +
-                                    "  \"dish_id\": \"762dbe8e-4cfc-40f0-af3f-02cfc67da709\"\n" +
-                                    "}"
+                            """
+                                    {
+                                      "dish_id": "762dbe8e-4cfc-40f0-af3f-02cfc67da709"
+                                    }"""
                     ))),
             @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(mediaType = "application/json",
                     schema = @Schema(example =
-                            "{\n" +
-                                    "    \"timestamp\": \"2023-08-23T08:06:10.306+00:00\",\n" +
-                                    "    \"status\": 400,\n" +
-                                    "    \"error\": \"Bad Request\",\n" +
-                                    "    \"message\": \"400 BAD_REQUEST\",\n" +
-                                    "    \"path\": \"/dishes/\",\n" +
-                                    "    \"fields\": {\n" +
-                                    "        \"category_id\": \"Category with id null not found\",\n" +
-                                    "        \"price\": \"Price cannot be empty\",\n" +
-                                    "        \"name\": \"Field name is required\",\n" +
-                                    "        \"state\": \"The state is incorrect\"\n" +
-                                    "    }\n" +
-                                    "}"
+                            """
+                                    {
+                                        "timestamp": "2023-08-23T08:06:10.306+00:00",
+                                        "status": 400,
+                                        "error": "Bad Request",
+                                        "message": "400 BAD_REQUEST",
+                                        "path": "/dishes/",
+                                        "fields": {
+                                            "category_id": "Category with id null not found",
+                                            "price": "Price cannot be empty",
+                                            "name": "Field name is required",
+                                            "state": "The state is incorrect"
+                                        }
+                                    }"""
                     )))
     })
     public ResponseEntity<DishResponseId> createDish(@RequestBody
-                                                     @Schema(example = "{\n" +
-                                                             "  \"category_id\": \"uuid\",\n" +
-                                                             "  \"name\": \"string\",\n" +
-                                                             "  \"description\": \"string\",\n" +
-                                                             "  \"price\": 1.99,\n" +
-                                                             "  \"weight\": 0,\n" +
-                                                             "  \"calories\": 0,\n" +
-                                                             "    \"allergens\": [\n" +
-                                                             "        \"Celery\",\n" +
-                                                             "        \"Cereals containing gluten\",\n" +
-                                                             "        \"Clams\"\n" +
-                                                             "    ],\n" +
-                                                             "    \"tags\": [\n" +
-                                                             "        \"Spicy\",\n" +
-                                                             "        \"Vegan\"\n" +
-                                                             "    ],\n" +
-                                                             "  \"state\": \"ENABLED or DISABLED\",\n" +
-                                                             "  \"image\": \"Base64 only JPG and PNG (not necessary)\"\n" +
-                                                             "}")
+                                                     @Schema(example = """
+                                                             {
+                                                               "category_id": "uuid",
+                                                               "name": "string",
+                                                               "description": "string",
+                                                               "price": 1.99,
+                                                               "weight": 0,
+                                                               "calories": 0,
+                                                                 "allergens": [
+                                                                     "Celery",
+                                                                     "Cereals containing gluten",
+                                                                     "Clams"
+                                                                 ],
+                                                                 "tags": [
+                                                                     "Spicy",
+                                                                     "Vegan"
+                                                                 ],
+                                                               "state": "ENABLED or DISABLED",
+                                                               "images": "Base64 only JPG and PNG (not necessary)"
+                                                             }""")
                                                      Dish request) {
         DishResponseId response = service.createDish(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
@@ -115,35 +120,37 @@ public class DishController {
             @ApiResponse(responseCode = "204", description = "No Content"),
             @ApiResponse(responseCode = "404", description = "Not found", content = @Content(mediaType = "application/json",
                     schema = @Schema(example =
-                            "{\n" +
-                                    "    \"timestamp\": \"2023-08-23T08:12:06.421+00:00\",\n" +
-                                    "    \"status\": 404,\n" +
-                                    "    \"error\": \"Not Found\",\n" +
-                                    "    \"message\": \"Dish with id cb18dfad-24bx-4a1c-aa84-c48bf356a3b6 not found\",\n" +
-                                    "    \"path\": \"/dishes/cb18dfad-24bx-4a1c-aa84-c48bf356a3b6\"\n" +
-                                    "}"
+                            """
+                                    {
+                                        "timestamp": "2023-08-23T08:12:06.421+00:00",
+                                        "status": 404,
+                                        "error": "Not Found",
+                                        "message": "Dish with id cb18dfad-24bx-4a1c-aa84-c48bf356a3b6 not found",
+                                        "path": "/dishes/cb18dfad-24bx-4a1c-aa84-c48bf356a3b6"
+                                    }"""
                     )))
     })
     public ResponseEntity<Void> patchDishById(@PathVariable String dishId, @RequestBody
-    @Schema(example = "{\n" +
-            "  \"category_id\": \"uuid\",\n" +
-            "  \"name\": \"string\",\n" +
-            "  \"description\": \"string\",\n" +
-            "  \"price\": 1.99,\n" +
-            "  \"weight\": 0,\n" +
-            "  \"calories\": 0,\n" +
-            "    \"allergens\": [\n" +
-            "        \"Celery\",\n" +
-            "        \"Cereals containing gluten\",\n" +
-            "        \"Clams\"\n" +
-            "    ],\n" +
-            "    \"tags\": [\n" +
-            "        \"Spicy\",\n" +
-            "        \"Vegan\"\n" +
-            "    ],\n" +
-            "  \"state\": \"ENABLED or DISABLED\",\n" +
-            "  \"image\": \"Base64 only JPG and PNG (not necessary)\"\n" +
-            "}")
+    @Schema(example = """
+            {
+              "category_id": "uuid",
+              "name": "string",
+              "description": "string",
+              "price": 1.99,
+              "weight": 0,
+              "calories": 0,
+                "allergens": [
+                    "Celery",
+                    "Cereals containing gluten",
+                    "Clams"
+                ],
+                "tags": [
+                    "Spicy",
+                    "Vegan"
+                ],
+              "state": "ENABLED or DISABLED",
+              "images": "Base64 only JPG and PNG (not necessary)"
+            }""")
     Dish request) {
         service.patchDishById(dishId, request);
         return ResponseEntity.noContent().build();
@@ -154,13 +161,14 @@ public class DishController {
             @ApiResponse(responseCode = "204", description = "No Content"),
             @ApiResponse(responseCode = "404", description = "Not found", content = @Content(mediaType = "application/json",
                     schema = @Schema(example =
-                            "{\n" +
-                                    "    \"timestamp\": \"2023-08-23T08:13:36.104+00:00\",\n" +
-                                    "    \"status\": 404,\n" +
-                                    "    \"error\": \"Not Found\",\n" +
-                                    "    \"message\": \"Dish with id c8e7375d-1dbf-4d40-ae65-x1cd1e6e973f not found\",\n" +
-                                    "    \"path\": \"/dishes/c8e7375d-1dbf-4d40-ae65-x1cd1e6e973f\"\n" +
-                                    "}"
+                            """
+                                    {
+                                        "timestamp": "2023-08-23T08:13:36.104+00:00",
+                                        "status": 404,
+                                        "error": "Not Found",
+                                        "message": "Dish with id c8e7375d-1dbf-4d40-ae65-x1cd1e6e973f not found",
+                                        "path": "/dishes/c8e7375d-1dbf-4d40-ae65-x1cd1e6e973f"
+                                    }"""
                     )))
     })
     public ResponseEntity<Void> deleteDishById(@PathVariable String dishId) {
