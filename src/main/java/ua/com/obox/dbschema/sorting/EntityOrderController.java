338f14762b5104a684f41f0631a91c25865b5ef6
nemeritskyy <85497703+nemeritskyy@users.noreply.github.com>
2023-10-15 11:16:07 +0300

    refactor and add validation for list of uuid

--- src/main/java/ua/com/obox/dbschema/sorting/EntityOrderController.java
+++ src/main/java/ua/com/obox/dbschema/sorting/EntityOrderController.java
@@ -10,6 +10,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import static ua.com.obox.dbschema.tools.examples.OrderResponseExample.*;
+
 @RestController
 @RequestMapping("/order/")
 @RequiredArgsConstructor
@@ -21,34 +23,11 @@ public class EntityOrderController {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "204", description = "No Content"),
             @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(mediaType = "application/json",
-                    schema = @Schema(example = "{\n" +
-                            "    \"timestamp\": \"2023-10-13T13:13:40.233+00:00\",\n" +
-                            "    \"status\": 400,\n" +
-                            "    \"error\": \"Bad Request\",\n" +
-                            "    \"message\": \"400 BAD_REQUEST\",\n" +
-                            "    \"path\": \"/order/\",\n" +
-                            "    \"fields\": {\n" +
-                            "        \"reference_type\": \"Bad reference type\"\n" +
-                            "    }\n" +
-                            "}"))),
+                    schema = @Schema(example = POST_400_RESPONSE_EXAMPLE))),
             @ApiResponse(responseCode = "404", description = "Not found", content = @Content(mediaType = "application/json",
-                    schema = @Schema(example = "{\n" +
-                            "    \"timestamp\": \"2023-10-13T13:02:31.586+00:00\",\n" +
-                            "    \"status\": 404,\n" +
-                            "    \"error\": \"Not Found\",\n" +
-                            "    \"message\": \"When trying to refresh the sorted list, the entity was not found\",\n" +
-                            "    \"path\": \"/order/\"\n" +
-                            "}")))
+                    schema = @Schema(example = POST_404_RESPONSE_EXAMPLE)))
     })
-    public ResponseEntity<Void> createMenu(@RequestBody @Schema(example = "{\n" +
-            "    \"entity_id\":\"f08de446-7622-4c3-b5bd-a4da773ca3274\",\n" +
-            "    \"reference_type\":\"CATEGORY\",\n" +
-            "    \"sorted_list\": [\n" +
-            "        \"2cbd12a3-9f7d-458b-8509-91bf6cc5b190\",\n" +
-            "        \"544f8740-9574-4be7-8633-38d29b508554\",\n" +
-            "        \"1f1679bd-7c0e-4645-b70e-83473f70da81\"\n" +
-            "    ]\n" +
-            "}") EntityOrder request, @RequestHeader HttpHeaders httpHeaders) {
+    public ResponseEntity<Void> createMenu(@RequestBody @Schema(example = POST_REQUEST_BODY_EXAMPLE) EntityOrder request, @RequestHeader HttpHeaders httpHeaders) {
         String acceptLanguage = httpHeaders.getFirst("Accept-Language");
         service.createEntityOrder(request, acceptLanguage);
         return ResponseEntity.noContent().build();
--- src/main/java/ua/com/obox/dbschema/sorting/EntityOrderService.java
+++ src/main/java/ua/com/obox/dbschema/sorting/EntityOrderService.java
@@ -1,7 +1,6 @@
 package ua.com.obox.dbschema.sorting;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import ua.com.obox.dbschema.category.CategoryRepository;
@@ -25,71 +24,62 @@ public class EntityOrderService {
     private final RestaurantRepository restaurantRepository;
     private final CategoryRepository categoryRepository;
     private final LoggingService loggingService;
-    private final ResourceBundle translation = ResourceBundle.getBundle("translation.messages");
+    private static final ResourceBundle translation = ResourceBundle.getBundle("translation.messages");
 
     public void createEntityOrder(EntityOrder request, String acceptLanguage) {
         String finalAcceptLanguage = CheckHeader.checkHeaderLanguage(acceptLanguage);
         String entityUUID = request.getEntityId();
         Map<String, String> fieldErrors = new ResponseErrorMap<>();
-        EntityOrder entityOrderExist = entityOrderRepository.findByEntityId(entityUUID).orElseGet(() -> {
-            String[] validValues = {"MENU", "CATEGORY", "DISH"};
-            Map<String, JpaRepository<?, ?>> entityList = new HashMap<>(); // reference for parent entity
-            entityList.put("MENU", restaurantRepository);
-            entityList.put("CATEGORY", menuRepository);
-            entityList.put("DISH", categoryRepository);
-            if (Arrays.asList(validValues).contains(request.getReferenceType())) {
-                JpaRepository<?, ?> repository = entityList.get(request.getReferenceType());
-                if (repository instanceof RestaurantRepository restaurantRepo) {
-                    var restaurantInfo = restaurantRepo.findByRestaurantId(entityUUID);
-                    try {
-                        checkEntityInDatabase(restaurantInfo, request, finalAcceptLanguage);
-                    } catch (Throwable e) {
-                        throw new RuntimeException(e);
-                    }
-                } else if (repository instanceof MenuRepository menuRepo) {
-                    var menuInfo = menuRepo.findByMenuId(entityUUID);
-                    try {
-                        checkEntityInDatabase(menuInfo, request, finalAcceptLanguage);
-                    } catch (Throwable e) {
-                        throw new RuntimeException(e);
-                    }
-                } else if (repository instanceof CategoryRepository categoryRepo) {
-                    var categoryInfo = categoryRepo.findByCategoryId(entityUUID);
-                    try {
-                        checkEntityInDatabase(categoryInfo, request, finalAcceptLanguage);
-                    } catch (Throwable e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            } else {
-                fieldErrors.put("reference_type", translation.getString(finalAcceptLanguage + ".badReferenceType"));
-            }
-            return null;
-        });
+        if (!String.join(",", request.getSortedArray()).matches("([0-9a-fA-F-]{36}(,)?)+")) {
+            fieldErrors.put("sorted_list", translation.getString(finalAcceptLanguage + ".badSortedList"));
+        }
+        validationReference(request, fieldErrors, finalAcceptLanguage,
+                entityOrderRepository, restaurantRepository, menuRepository, categoryRepository); // validate reference
 
         if (fieldErrors.size() > 0)
             throw new BadFieldsResponse(HttpStatus.BAD_REQUEST, fieldErrors);
 
-        //update
-        if (entityOrderExist != null) {
-            entityOrderExist.setSortedArray(request.getSortedArray(), entityOrderRepository);
-            entityOrderExist.setUpdatedAt(Instant.now().getEpochSecond());
-            entityOrderRepository.save(entityOrderExist);
-        }
-
         loggingService.log(LogLevel.INFO, String.format("Update/Create sorting list for %s UUID=%s", request.getReferenceType(), entityUUID));
     }
 
-    private void checkEntityInDatabase(Optional<?> foundEntity, EntityOrder request, String acceptLanguage) {
-        foundEntity.orElseGet(() -> {
+    private static void checkEntityInDatabase(Optional<?> foundEntity, EntityOrder request, String acceptLanguage, EntityOrderRepository entityOrderRepository, RestaurantRepository restaurantRepository, MenuRepository menuRepository, CategoryRepository categoryRepository) {
+        if (foundEntity.isEmpty()){
             ExceptionTools.notFoundResponse(".entityOrderNotFound", acceptLanguage, request.getEntityId());
-            return null;
-        });
+        }
 
-        request.setSortedArray(request.getSortedArray(), entityOrderRepository);
-        request.setCreatedAt(Instant.now().getEpochSecond());
-        request.setUpdatedAt(Instant.now().getEpochSecond());
-        entityOrderRepository.save(request);
+        EntityOrder entityOrderExist = entityOrderRepository.findByEntityId(request.getEntityId()).orElse(null);
+        EntityOrder entityOrderToSave;
+
+        if (entityOrderExist != null) {
+            entityOrderExist.setSortedArray(request.getSortedArray(), entityOrderRepository);
+            entityOrderExist.setUpdatedAt(Instant.now().getEpochSecond());
+            entityOrderToSave = entityOrderExist;
+        } else {
+            request.setSortedArray(request.getSortedArray(), entityOrderRepository);
+            request.setCreatedAt(Instant.now().getEpochSecond());
+            request.setUpdatedAt(Instant.now().getEpochSecond());
+            entityOrderToSave = request;
+        }
+        entityOrderRepository.save(entityOrderToSave);
     }
 
+    private static void validationReference(EntityOrder request, Map<String, String> fieldErrors, String finalAcceptLanguage,
+                                            EntityOrderRepository entityOrderRepository, RestaurantRepository restaurantRepository, MenuRepository menuRepository, CategoryRepository categoryRepository) {
+        switch (request.getReferenceType()) {
+            case "DISH" -> {
+                var categoryInfo = categoryRepository.findByCategoryId(request.getEntityId());
+                checkEntityInDatabase(categoryInfo, request, finalAcceptLanguage, entityOrderRepository, restaurantRepository, menuRepository, categoryRepository);
+            }
+            case "CATEGORY" -> {
+                var menuInfo = menuRepository.findByMenuId(request.getEntityId());
+                checkEntityInDatabase(menuInfo, request, finalAcceptLanguage, entityOrderRepository, restaurantRepository, menuRepository, categoryRepository);
+            }
+            case "MENU" -> {
+                var restaurantInfo = restaurantRepository.findByRestaurantId(request.getEntityId());
+                checkEntityInDatabase(restaurantInfo, request, finalAcceptLanguage, entityOrderRepository, restaurantRepository, menuRepository, categoryRepository);
+            }
+            default ->
+                    fieldErrors.put("reference_type", translation.getString(finalAcceptLanguage + ".badReferenceType"));
+        }
+    }
 }
