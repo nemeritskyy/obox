2e90035a4e38e0cef4477fa396d1c4cacfc9d783
nemeritskyy <85497703+nemeritskyy@users.noreply.github.com>
2023-10-24 08:01:48 +0300

    add language entity for the structure

---
+++ src/main/java/ua/com/obox/dbschema/language/LanguageController.java
@@ -0,0 +1,25 @@
+package ua.com.obox.dbschema.language;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/languages")
+@RequiredArgsConstructor
+@Tag(name = "Languages")
+public class LanguageController {
+    private final LanguageService service;
+
+    @GetMapping("/{tenantId}")
+    public ResponseEntity<List<Language>> getAllLanguagesByTenantId(@PathVariable String tenantId,
+                                                                    @RequestHeader HttpHeaders httpHeaders) {
+        String acceptLanguage = httpHeaders.getFirst("Accept-Language");
+        List<Language> getAllLanguagesByTenantId = service.getAllLanguages(tenantId, acceptLanguage);
+        return ResponseEntity.ok(getAllLanguagesByTenantId);
+    }
+}
---
+++ src/main/java/ua/com/obox/dbschema/language/LanguageRepository.java
@@ -0,0 +1,10 @@
+package ua.com.obox.dbschema.language;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface LanguageRepository extends JpaRepository<Language, UUID> {
+    List<Language> findAllByTenant_TenantId(String tenantId);
+}
