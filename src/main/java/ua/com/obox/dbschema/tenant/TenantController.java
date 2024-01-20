c847f440f7c8fc6b333a1db56426f5e79bfd804b
nemeritskyy <85497703+nemeritskyy@users.noreply.github.com>
2024-01-20 11:25:49 +0200

    check permission for tenant endpoints

--- src/main/java/ua/com/obox/dbschema/tenant/TenantController.java
+++ src/main/java/ua/com/obox/dbschema/tenant/TenantController.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import ua.com.obox.dbschema.restaurant.RestaurantResponse;
 
@@ -55,6 +56,7 @@ public class TenantController {
     }
 
     @PostMapping("/")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "Success", content = @Content(mediaType = "application/json",
                     schema = @Schema(example = POST_201_RESPONSE_EXAMPLE))),
@@ -82,6 +84,7 @@ public class TenantController {
     }
 
     @DeleteMapping("/{tenantId}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "204", description = "No Content"),
             @ApiResponse(responseCode = "404", description = "Not found", content = @Content(mediaType = "application/json",
