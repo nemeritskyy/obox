0bac7cb5b327eff6d5347d6642cfe720ace628d0
nemeritskyy <85497703+nemeritskyy@users.noreply.github.com>
2024-01-21 15:39:12 +0200

    check permission for mark endpoints

--- src/main/java/ua/com/obox/authserver/user/UserService.java
+++ src/main/java/ua/com/obox/authserver/user/UserService.java
@@ -1,6 +1,7 @@
 package ua.com.obox.authserver.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -10,6 +11,8 @@ import ua.com.obox.dbschema.category.Category;
 import ua.com.obox.dbschema.category.CategoryRepository;
 import ua.com.obox.dbschema.dish.Dish;
 import ua.com.obox.dbschema.dish.DishRepository;
+import ua.com.obox.dbschema.mark.Mark;
+import ua.com.obox.dbschema.mark.MarkRepository;
 import ua.com.obox.dbschema.menu.Menu;
 import ua.com.obox.dbschema.menu.MenuRepository;
 import ua.com.obox.dbschema.restaurant.Restaurant;
@@ -18,10 +21,9 @@ import ua.com.obox.dbschema.tenant.Tenant;
 import ua.com.obox.dbschema.tenant.TenantRepository;
 import ua.com.obox.dbschema.tools.attachment.ReferenceType;
 import ua.com.obox.dbschema.tools.exception.ExceptionTools;
+import ua.com.obox.dbschema.tools.response.BadFieldsResponse;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -31,6 +33,9 @@ public class UserService {
     private final MenuRepository menuRepository;
     private final RestaurantRepository restaurantRepository;
     private final TenantRepository tenantRepository;
+    private final MarkRepository markRepository;
+
+    private static final ResourceBundle translation = ResourceBundle.getBundle("translation.messages");
     private final Map<String, String> accessCache = new HashMap<>();
 
     public void checkPermissionForUser(ReferenceType referenceType, String requestToEntityId, String acceptLanguage) {
@@ -70,6 +75,12 @@ public class UserService {
                                 .map(Tenant::getTenantId)
                                 .orElse(null);
                     }
+                    case mark -> {
+                        Optional<Mark> checkMarkAccess = markRepository.findByMarkId(requestToEntityId);
+                        tenantIdFromEntity = checkMarkAccess
+                                .map(mark -> Objects.requireNonNull(restaurantRepository.findByRestaurantId(mark.getReferenceId()).orElse(null)).getTenant().getTenantId())
+                                .orElse(null);
+                    }
                     default -> throwNotFound(ReferenceType.entity, requestToEntityId, acceptLanguage);
                 }
                 System.out.println("tenant from entity " + tenantIdFromEntity);
@@ -84,6 +95,16 @@ public class UserService {
                 if (!accessCache.get(requestToEntityId).equals(tenantId))
                     throwNotFound(referenceType, requestToEntityId, acceptLanguage);
             }
+        }
+    }
+
+    public void checkPermissionForUser(ReferenceType referenceType, String requestToEntityId, String referenceInputType, String acceptLanguage) {
+        if (referenceInputType.equals(referenceType.toString())) {
+            checkPermissionForUser(referenceType, requestToEntityId, acceptLanguage);
+        } else {
+            Map<String, String> errors = new HashMap<>();
+            errors.put("reference_type", translation.getString(acceptLanguage + ".badReferenceType"));
+            throw new BadFieldsResponse(HttpStatus.BAD_REQUEST, errors);
         }
     }
 
